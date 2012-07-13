e0cba965fabcc58f164360eab2d7e992089f94be
sergiocormio <sergiocormio@1aa6c546-6dda-c317-fe2a-0de851242324>
2012-07-13 17:29:45 +0000

    DocumentTypeService first implementation

---
+++ trunk/server/src/com/cdz/sh/service/DocumentTypeService.java
@@ -0,0 +1,13 @@
+package com.cdz.sh.service;
+
+import com.cdz.sh.model.DocumentType;
+
+/**
+ * Facade for DocumentType entity
+ * 
+ * @author sergiocormio
+ *
+ */
+public interface DocumentTypeService extends CrudService<DocumentType, Long>{
+
+}
