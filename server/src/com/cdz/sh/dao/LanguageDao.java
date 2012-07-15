c8009d687e4c50d52ef6eb460f3b8adefac63541
federico.deseta <federico.deseta@gmail.com>
2012-07-15 20:24:36 +0000

    Language + lastLodgementDate bien escrito + test updateado

---
+++ server/src/com/cdz/sh/dao/LanguageDao.java
@@ -0,0 +1,18 @@
+package com.cdz.sh.dao;
+
+import com.cdz.sh.dao.crud.CrudDao;
+import com.cdz.sh.model.Language;
+
+/**
+ * Declares specific functionality for customers access data, in addition to the CRUD methods.
+ * 
+ * @author fede
+ *
+ */
+public interface LanguageDao extends CrudDao<Language, Long> {
+
+	/**
+	 * TODO: declare specific query methods
+	 */
+
+}
