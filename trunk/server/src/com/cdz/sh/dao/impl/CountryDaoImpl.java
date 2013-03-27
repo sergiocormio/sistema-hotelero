139a28deb6baa2261131a954f3fd78abffb51891
federico.deseta@gmail.com <federico.deseta@gmail.com@1aa6c546-6dda-c317-fe2a-0de851242324>
2013-03-27 02:39:19 +0000

    Agrego un servicio para Country (que por el momento no estoy usando y no creo usar)

--- trunk/server/src/com/cdz/sh/dao/impl/CountryDaoImpl.java
+++ trunk/server/src/com/cdz/sh/dao/impl/CountryDaoImpl.java
@@ -1,7 +1,16 @@
 package com.cdz.sh.dao.impl;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceException;
+import javax.persistence.TypedQuery;
+
 import com.cdz.sh.dao.CountryDao;
 import com.cdz.sh.dao.crud.AbstractCrudDao;
+import com.cdz.sh.dao.crud.EntityManagerFactorySingleton;
+import com.cdz.sh.dao.exception.DaoException;
 import com.cdz.sh.model.Country;
 
 /**
@@ -13,10 +22,29 @@ import com.cdz.sh.model.Country;
  */
 public class CountryDaoImpl extends AbstractCrudDao<Country, Long> implements CountryDao {
 
-	/**
-	 * TODO implement specific queries
-	 */
-
-
 	
+	public List<Country> retrieveCountriesWithoutRegions() throws DaoException {
+		
+		EntityManagerFactory entityManagerFactory = EntityManagerFactorySingleton.getInstance();
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		try {
+			entityManager.getTransaction().begin();
+			
+			String strQuery = "SELECT c FROM Country c WHERE c not in (SELECT c2 from Region r, Country c2 WHERE r.country = c2)";
+			
+			TypedQuery<Country> query = entityManager.createQuery(strQuery, Country.class);
+			
+			List<Country> countriesWithoutRegions = query.getResultList();
+			entityManager.getTransaction().commit();
+			return countriesWithoutRegions;
+		}
+		catch(PersistenceException persistenceException){
+			throw new DaoException(persistenceException.getMessage());
+		}
+		finally{
+			entityManager.close();
+		}
+
+		
+	}
 }
--- trunk/server/src/com/cdz/sh/service/impl/CountryServiceImpl.java
+++ trunk/server/src/com/cdz/sh/service/impl/CountryServiceImpl.java
@@ -1,6 +1,10 @@
 package com.cdz.sh.service.impl;
 
+import java.util.List;
+
+import com.cdz.sh.dao.CountryDao;
 import com.cdz.sh.dao.crud.CrudDao;
+import com.cdz.sh.dao.exception.DaoException;
 import com.cdz.sh.dao.impl.CountryDaoImpl;
 import com.cdz.sh.model.Country;
 import com.cdz.sh.service.AbstractCrudService;
@@ -14,9 +18,17 @@ import com.cdz.sh.service.CountryService;
  */
 public class CountryServiceImpl extends AbstractCrudService<Country, Long> implements CountryService {
 
+	private CountryDao countryDao;
+	
 	@Override
 	protected CrudDao<Country, Long> createDao() {
-		return new CountryDaoImpl();
+		this.countryDao = new CountryDaoImpl();
+		return this.countryDao;
+	}
+
+	@Override
+	public List<Country> retrieveCountriesWithoutRegions() throws DaoException {
+		return this.countryDao.retrieveCountriesWithoutRegions();
 	}
 	
 
