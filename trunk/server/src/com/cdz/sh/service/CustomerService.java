ae930fcfe1e84712b2b0ef836ddc3a9f1a7de879
federico.deseta@gmail.com <federico.deseta@gmail.com@1aa6c546-6dda-c317-fe2a-0de851242324>
2013-07-28 23:22:33 +0000

    se agrega metodo en customerService para filtrar customer por email nombre apellido y country, los 4 filtros son opcionales

--- trunk/server/src/com/cdz/sh/service/CustomerService.java
+++ trunk/server/src/com/cdz/sh/service/CustomerService.java
@@ -3,6 +3,8 @@ package com.cdz.sh.service;
 import java.util.List;
 
 import com.cdz.sh.dao.exception.DaoException;
+import com.cdz.sh.dao.exception.InvalidParameterException;
+import com.cdz.sh.model.Country;
 import com.cdz.sh.model.Customer;
 import com.cdz.sh.model.Region;
 
@@ -16,6 +18,8 @@ public interface CustomerService extends CrudService<Customer, Long>{
 	
 	
 
+	public List<Customer> retrieveCustomers(String email, String firstName, String lastName, Country country) throws InvalidParameterException, DaoException;
+	
 	public List<Customer> retrieveCustomers(List<Region> region, List<Integer> months,
 											boolean includeCustomersWithoutRegion,
 											boolean includeCustomersWithoutBirthdate) throws DaoException;
